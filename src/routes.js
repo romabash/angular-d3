bfcfcfed4d55749415f8f9b0ce712d43e4e136da
Roma <romanofkin@gmail.com>
2017-08-11 21:16:35 -0700

    Change main-display to bar-display as component

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,13 @@
 
 (function(){
 	'use strict';
-	
+
 	angular.module('App')
 	.config(RoutesConfig);
-	
+
 	RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
 	function RoutesConfig($stateProvider, $urlRouterProvider){
-	  
+
       //Redirect to home page if no other URL matches
       $urlRouterProvider.otherwise('/');
 
@@ -17,14 +17,10 @@
       //Home page
       .state('home', {
 		url: '/',
-		template:'<main-display data="app.barData"></main-display>',
+		template:'<bar-display data="app.barData"></bar-display>',
 		controller: 'MainCtrl as app'
 	  });
-	  
+
 	}
-	
+
 })();
-
-
-
-
