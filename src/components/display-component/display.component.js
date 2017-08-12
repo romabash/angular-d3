fd10507813a9c9835ff66fb8ee1a329914591a21
Roma <romanofkin@gmail.com>
2017-08-11 20:55:28 -0700

    Update display.component to use index for color scale

--- src/components/display-component/display.component.js
+++ src/components/display-component/display.component.js
@@ -1,19 +1,19 @@
 
 (function(){
   'use strict';
-	
+
   angular.module('App')
   .component('mainDisplay', {
 	templateUrl: 'src/components/display-component/display-component.html',
 	controller: MainDisplayController,
 	bindings: {
 		data: '<' // in routes: data=app.barData.  When using $ctrl.data in template or controller, refering to app.barData
-	} 
+	}
   });
-  
+
 //Component Controller - Display
-  MainDisplayController.$inject = ['d3Service', '$window', '$element', '$rootScope'];
-  function MainDisplayController(d3Service, $window, $element, $rootScope){
+  MainDisplayController.$inject = ['d3Service', '$window', '$element'];
+  function MainDisplayController(d3Service, $window, $element){
 	var $ctrl = this;
 
 	d3Service.d3().then(function(d3) {
@@ -21,34 +21,35 @@
 		var margin = 20;
 		var barHeight = 20;
 		var barPadding = 5;
-		
+
 		$window.onresize = function() {
 			$ctrl.render();
 		};
-		
+
 		var canvas = d3.select('#bars')
 			.append('svg')
 			.style('width', '100%');
 			//.style('border', '1px solid black');
-	
+
 		$ctrl.render = function() {
 			//remove all previous items before render
 			canvas.selectAll('*').remove();
-		
+
 			//set up width based on the size of the div
 			var width = d3.select("#bars").node().offsetWidth - margin;
-			// calculate the height
+			// calculate the height of canvas
 			var height = $ctrl.data.length * (barHeight + barPadding);
 			// Use the schemeCategory10 scale function for multicolor support
 			var color = d3.scaleOrdinal(d3.schemeCategory10);
-			// set up xScale - based on highest score/the size of the window
+
+      // set up xScale - based on highest score/the size of the window
 			var xScale = d3.scaleLinear()
 				.domain([0, d3.max($ctrl.data, function(d) { return d.score; }) ]) //max score
 				.range([0, width]);
-				
+
 			// set the height based on the calculations above
 			canvas.attr('height', height);
-			
+
 			//create the rectangles for the bar chart
 			canvas.selectAll('rect')
 			.data($ctrl.data).enter()
@@ -59,24 +60,25 @@
 				.attr('y', function(d,i) {
 					return i * (barHeight + barPadding);
 				})
-				.attr('fill', function(d) { return color(d.score); })
+				.attr('fill', function(d, i) { return color(i); })
 				.transition()
 				.duration(1000)
 				.attr('width', function(d) {
 					return xScale(d.score);
 				});
-		} //Ends render function
+
+    } //Ends render function
 
 		$ctrl.$doCheck = function(){
 			$ctrl.render();
 		};
-		
+
     }); //Ends d3 promise
-	
+
 	$ctrl.$doCheck = function(){
 
 	};
-	
+
   }
-	
+
 })();
